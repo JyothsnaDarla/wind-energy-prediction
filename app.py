9311cba7cc5185b9c5ce76407ab597a8ef31147a
Jyothsna Darla <jyothsnadarla464@gmail.com>
2026-02-13 12:33:07 +0530

    Update app.run to use environment variables for host and port

--- app.py
+++ app.py
@@ -61,5 +61,8 @@ def predict():
                            prediction=round(prediction[0],2))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
